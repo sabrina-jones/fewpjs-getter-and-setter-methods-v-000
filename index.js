962dc74c777af3d89c913395601737e89432940c
Sabrina Jones <sabrina.jones87@gmail.com>
2021-03-11 01:59:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your Circle class here
+class Circle {
+    this.radius = circumference / (pi * 2)
+  }
+
+  set area(area){
+    this.radius = Math.sqrt(area / pi)
+  }
+
+  get diameter(){
+    return this.radius * 2
+  }
